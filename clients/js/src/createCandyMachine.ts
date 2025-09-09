e2ee7f6c2737482c87aa803b9de1b41ae10e4c7f
Tony Boyle <tony@tonyboyle.co.uk>
2025-09-09 11:04:41 +0100

    fixes candymachine creation with correct payer.

--- clients/js/src/createCandyMachine.ts
+++ clients/js/src/createCandyMachine.ts
@@ -54,6 +54,7 @@ export const createCandyMachine = async (
   return transactionBuilder()
     .add(
       createAccount(context, {
+        payer: newInput.payer ?? undefined,
         newAccount: newInput.candyMachine,
         lamports,
         space,
