e7c635b8d3ed3a0a13496b95438559dc893cb74c
Tony Boyle <tony@tonyboyle.co.uk>
2026-03-25 13:17:54 +0000

    make explicit

--- clients/js/src/createCandyMachine.ts
+++ clients/js/src/createCandyMachine.ts
@@ -54,7 +54,7 @@ export const createCandyMachine = async (
   return transactionBuilder()
     .add(
       createAccount(context, {
-        payer: newInput.payer,
+        payer: newInput.payer ?? context.payer,
         newAccount: newInput.candyMachine,
         lamports,
         space,
