6903e8cac71467249f875cc5d44f0b0eee1050fc
stranzhay <stranzhay@users.noreply.github.com>
2026-05-20 00:07:45 +0000

    Update generated clients

--- programs/candy-machine-core/program/src/lib.rs
+++ programs/candy-machine-core/program/src/lib.rs
@@ -13,7 +13,7 @@ mod instructions;
 mod state;
 mod utils;
 
-declare_id!("CMACYFENjoBMHzapRXyo1JZkVS6EtaDDzkjMrmQLvr4J");
+declare_id!("F8FnAZFh3UU8YpUrtb5A3xXPYmYVn63ooMqedWX7y3SS");
 
 #[cfg(not(feature = "no-entrypoint"))]
 solana_security_txt::security_txt! {
